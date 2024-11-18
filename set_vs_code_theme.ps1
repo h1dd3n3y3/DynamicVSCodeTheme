5d3d4295e9a2ae5f844f70e7f1fdfd4ab3f0cf20
David Parathyras <parathyrasdavid@gmail.com>
2024-11-18 10:49:34 +0200

    Update set_vs_code_theme.ps1

--- set_vs_code_theme.ps1
+++ set_vs_code_theme.ps1
@@ -10,45 +10,45 @@ $content_custom_object = Get-Content -Raw $settings_json | ConvertFrom-Json # Co
 
 $current_theme = $content_custom_object.'workbench.colorTheme' # Get current theme
 
-$prefered_dark_theme = $content_custom_object.'workbench.preferredDarkColorTheme' # Get prefered dark theme
-$prefered_light_theme = $content_custom_object.'workbench.preferredLightColorTheme' # Get prefered light theme
+$preferred_dark_theme = $content_custom_object.'workbench.preferredDarkColorTheme' # Get preferred dark theme
+$preferred_light_theme = $content_custom_object.'workbench.preferredLightColorTheme' # Get preferred light theme
 
 
-if (($null -eq $prefered_dark_theme) -or ($null -eq $prefered_light_theme)) {
+if (($null -eq $preferred_dark_theme) -or ($null -eq $preferred_light_theme)) {
 
-    if ($null -eq $prefered_dark_theme) {
+    if ($null -eq $preferred_dark_theme) {
 
         $settings_json + ': ''workbench.preferredDarkColorTheme'' missing or has empty value.'
 
-    } elseif ($null -eq $prefered_light_theme) {
+    } elseif ($null -eq $preferred_light_theme) {
 
         $settings_json + ': ''workbench.preferredLightColorTheme'' missing or has empty value.'
     }
     
 } else {
 
-    if ($current_theme -eq $prefered_dark_theme) {
+    if ($current_theme -eq $preferred_dark_theme) {
 
         if (($current_hour -ge $light_theme_hour_start) -and ($current_hour -lt $dark_theme_hour_start)) {
             
             'Dark theme is enabled.'
-            'Changing to prefered light theme.'
+            'Changing to preferred light theme.'
         
             $src = '"workbench.colorTheme": ' + '"' + $current_theme + '"'
-            $dst = '"workbench.colorTheme": ' + '"' + $prefered_light_theme + '"'
+            $dst = '"workbench.colorTheme": ' + '"' + $preferred_light_theme + '"'
         
             $content.Replace($src, $dst) | Set-Content $settings_json
         }
 
-    } elseif ($current_theme -eq $prefered_light_theme) {
+    } elseif ($current_theme -eq $preferred_light_theme) {
 
         if (($current_hour -ge $dark_theme_hour_start) -or ($current_hour -lt $light_theme_hour_start)) {
 
             'Light theme is enabled.'
-            'Changing to prefered dark theme.'
+            'Changing to preferred dark theme.'
         
             $src = '"workbench.colorTheme": ' + '"' + $current_theme + '"'
-            $dst = '"workbench.colorTheme": ' + '"' + $prefered_dark_theme + '"'
+            $dst = '"workbench.colorTheme": ' + '"' + $preferred_dark_theme + '"'
 
             $content.Replace($src, $dst) | Set-Content $settings_json
         }
